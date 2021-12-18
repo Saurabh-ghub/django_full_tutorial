95f7076e8acb13edd4c65d61f9bc1b11b7a558df
Saurabh <saurabhkumar121201@gmail.com>
2021-12-18 16:26:23 +0530

    calculator using dtl

--- telusko/calc/views.py
+++ telusko/calc/views.py
@@ -4,3 +4,20 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request,"home.html",{'name':'Saurabh'})
+
+def add(request):
+    val1 = int(request.GET['num1'])
+    val2 = int(request.GET['num2'])
+    op = request.GET['operation']
+    if(op=='add'):
+        res = val1 + val2
+    elif op=='mul':
+        res = val1*val2
+    elif op=='sub':
+        res = val1-val2
+    else:
+        if val2 == 0:
+            res ="undefine"
+        else:
+            res = val1/val2
+    return render(request,"result.html",{'result' : res})
